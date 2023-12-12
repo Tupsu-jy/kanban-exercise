6a9a99a31a27e5f1c55cf6b27e18d37c075f2c6d
Tupsu-jy <ylinenjaakko@gmail.com>
2023-12-12 18:42:05 +0200

    A test setup added. A workflow added

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
+import { handleAxiosError } from './errorHandler'
 
 // Create an axios instance
 const instance = axios.create({
   //This does not actually directly access env variable,
   //that is done in webpack config. Should be renamed.
+  //osoite on ilmeisesti määritelty window.BASE_URL:ssa. process.env.BASE_URL,ei toimi
   baseURL: window.BASE_URL || process.env.BASE_URL,
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
@@ -16,7 +18,7 @@ instance.interceptors.response.use(
   (error) => {
     // Log any API call failures and propagate the error for further handling.
     console.error('API call failed:', error)
-    throw error
+    handleAxiosError // Käytä keskitettyä virheenkäsittelyä virheille
   }
 )
 
